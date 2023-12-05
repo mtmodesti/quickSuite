b6c5f958bda28bd0c5eba1cfd08acab0bb1f2b90
mtmodesti <marcelomodesti1988@gmail.com>
2023-12-05 16:34:03 -0300

    style: some style in movie dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,25 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MovieDashboardComponent } from './pages/movie-dashboard/movie-dashboard.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    MovieDashboardComponent
-    
-  ],
+  declarations: [AppComponent, HomeComponent, MovieDashboardComponent],
   imports: [
     MatIconModule,
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    MatTooltipModule,
+    MatSelectModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/movie-dashboard/movie-dashboard.component.ts
+++ src/app/pages/movie-dashboard/movie-dashboard.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-movie-dashboard',
   templateUrl: './movie-dashboard.component.html',
-  styleUrls: ['./movie-dashboard.component.scss']
+  styleUrls: ['./movie-dashboard.component.scss'],
 })
-export class MovieDashboardComponent {
+export class MovieDashboardComponent implements OnInit, AfterViewInit {
+  selectedMovie = history.state.movieInfo;
+  @ViewChild('movieImage') movieImage: ElementRef | undefined;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.handleMovieImage();
+  }
+
+  handleMovieImage() {
+    console.log(this.selectedMovie);
+    const htmlMoviePictureElement = this.movieImage;
+    console.log(htmlMoviePictureElement);
+    this.renderer.setStyle(
+      htmlMoviePictureElement?.nativeElement,
+      'background-image',
+      `url(${this.selectedMovie.url}`
+    );
+  }
 }
